1c68a0a9fe202050205fa1599dcaea57a9362bc3
Ritchie Fitzgerald <ritchiefitz1@gmail.com>
2018-08-22 09:11:50 -0500

    Added Hello World, and template for PrimitiveDataTypes examples.

---
+++ src/com/example/datatypes/PrimitiveDataTypes.java
@@ -0,0 +1,27 @@
+package com.example.datatypes;
+
+public class PrimitiveDataTypes {
+    private static void bool_examples() {
+        boolean example_bool = true;
+    }
+    private static void integer_examples() {
+        int example_int = 10;
+    }
+    private static void double_examples() {
+        double example_double = 10;
+    }
+    private static void character_examples() {
+        char example_char = 'C';
+    }
+    private static void string_examples() {
+        String example_string = "Ritchie";
+    }
+    public static void main(String[] args) {
+        bool_examples();
+        integer_examples();
+        double_examples();
+        character_examples();
+        string_examples();
+        System.out.println("Completed: PrimitiveDataTypes Fundamentals!");
+    }
+}
